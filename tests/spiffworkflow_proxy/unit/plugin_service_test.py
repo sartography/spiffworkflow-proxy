0acc02b13b27d6c3b2e5d55280df29fdafc0b68d
To Finke <tom.finke@webterra.de>
2025-05-22 20:33:00 +0200

    test: unit test for PluginService.describe_target

--- tests/spiffworkflow_proxy/unit/plugin_service_test.py
+++ tests/spiffworkflow_proxy/unit/plugin_service_test.py
@@ -20,3 +20,25 @@ def test_plugin_for_display_name() -> None:
 def test_available_commands_by_plugin() -> None:
     commands = PluginService.available_commands_by_plugin()
     assert(list(commands['connector_example'].keys()) == ['CombineStrings'])
+
+def test_describe_target() -> None:
+    commands = PluginService.available_commands_by_plugin()
+    combine_strings = commands["connector_example"]["CombineStrings"]
+    description = PluginService.describe_target(
+        "connector_example", "CombineStrings", combine_strings
+    )
+    assert description == {
+        "id": "example/CombineStrings",
+        "parameters": [
+            {
+                "id": "arg1",
+                "required": True,
+                "type": "str",
+            },
+            {
+                "id": "arg2",
+                "required": True,
+                "type": "str",
+            }
+        ]
+    }
